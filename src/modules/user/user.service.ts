fa372d7e15db17dcad0d93c490b1860ca42d0f61
liuliyi <wyang@shgbit.com>
2023-05-19 16:21:46 +0800

    🌈 style:好像写不了mongo的监听

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 import { Model, Schema } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -7,16 +7,7 @@ import { User, UserDocument } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>, @InjectConnection() private readonly connection: any) {
-    console.log('UserService constructor');
-    console.log(connection);
-    connection.on('close', () => {
-      console.log('UserService connection close');
-    });
-    setTimeout(() => {
-      connection.close();
-    }, 2000);
-  }
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     return await this.userModel.create(createUserDto);
