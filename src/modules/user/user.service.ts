e801606229161e43078f8a064b383a5ef50fd5ad
ruanchuhao <ruanchuhao@shgbit.com>
2023-05-19 16:05:18 +0800

    init: Mongo监听例子

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { Model, Schema } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User, UserDocument } from './entities/user.entity';
+
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>, @InjectConnection() private readonly connection: any) {
+    console.log('UserService constructor');
+    console.log(connection);
+    connection.on('close', () => {
+      console.log('UserService connection close');
+    });
+    setTimeout(() => {
+      connection.close();
+    }, 2000);
+  }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     return await this.userModel.create(createUserDto);
