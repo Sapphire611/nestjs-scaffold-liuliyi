c36c800d89e6277248b7feea79eb1f39f74dcbff
liuliyi <wyang@shgbit.com>
2023-06-01 11:06:33 +0800

    📃 docs:nothing

--- src/config/local.config.ts
+++ src/config/local.config.ts
@@ -1,12 +1,13 @@
 const mongodbName = 'nestjs-scaffold';
+const mongodbPort = 27018; // you may need change this port
 const ip = '127.0.0.1';
 
 export default {
   port: 19061,
   jwtSecret: 'QWZZuUjA',
   swaggerSuffix: 'api/swagger',
-  mongodbUrl: `mongodb://${ip}:27018/${mongodbName}`,
-  mongodbTestUrl: `mongodb://${ip}:27018/${mongodbName}-test`,
+  mongodbUrl: `mongodb://${ip}:${mongodbPort}/${mongodbName}`,
+  mongodbTestUrl: `mongodb://${ip}:${mongodbPort}/${mongodbName}-test`,
   redisUrl: `redis://${ip}:6379/6`,
   redisTestUrl: `redis://${ip}:6379/10`,
 };
